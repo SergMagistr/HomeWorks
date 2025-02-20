8d4ecaf62c74f94ec8df2d7ce66a049c14b12ddb
SergMagistr <kirilll-1963@mail.ru>
2025-02-20 19:27:29 +0300

    Добавьте файлы проекта.

---
+++ Task1_1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = 3;
+
+            // цикл 1 = 321
+            int t = c;
+            c = a;
+            a = t;
+
+            // цикл 2 = 312
+            int p = b;
+            b = c;
+            c = p;
+
+            Console.WriteLine("{0} {1} {2}", a, b, c);
+        }
+    }
+}
