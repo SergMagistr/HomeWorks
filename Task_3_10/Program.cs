cc67365e4095fcef31a580382ab25f7201b5a96b
SergMagistr <kirilll-1963@mail.ru>
2025-02-21 22:33:14 +0300

    Сделал ДЗ 3

---
+++ Task_3_10/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_3_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()); //  построил столько домов
+
+            if (n == 0) 
+            
+            {
+                Console.WriteLine($"ПИК построил {n} домов :(");
+            }
+
+            else if (n >1 && n <5 )
+
+            {
+                Console.WriteLine($"ПИК построил {n} дома :(");
+            }
+
+            else if (n > 4 )
+
+            {
+                Console.WriteLine($"ПИК построил {n} домов :)!!! ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Task_3_8/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task_3_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+
+
+            if (a > -10 && a < 10)
+            {
+                Console.WriteLine($"Да , число {a} попадает куда нужно ");
+
+            }
+            else if (!(a > -10 && a < 10))
+            {
+                Console.WriteLine($"Нет, число {a} не верное");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
