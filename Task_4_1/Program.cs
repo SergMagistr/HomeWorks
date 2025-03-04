3c2ad7f3cf7da9d7da2d12e885df3e2452c12237
SergMagistr <kirilll-1963@mail.ru>
2025-03-04 21:27:12 +0300

    Сделал ДЗ 4

---
+++ Task_4_1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // s = 1*2*3*4*5*6
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int s = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+             s = s * i;
+            }
+                Console.WriteLine(s);
+        }
+    }
+}
---
+++ Task_4_3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите степень");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > 0 || b > 0)
+
+            {
+                int s = 1;
+                for (int i = 0; i < b; i++)
+                {
+                    s = s * a;
+                }
+                Console.WriteLine($"{a} в степени {b}= {s} ") ; 
+            }
+
+            else 
+            {
+                Console.WriteLine("Ввведите положительные значения");
+            }
+
+        }
+    }
+}
